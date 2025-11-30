9e12931ce1ea2d84122b69f87b905a582ea4906d
alastago <48084924+alastago@users.noreply.github.com>
2025-11-30 23:34:06 +0700

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,17 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+def main_menu():
+    kb = [
+        [InlineKeyboardButton(text="Создать команду", callback_data="create_team")],
+        [InlineKeyboardButton(text="Вступить в команду", callback_data="join_team")],
+        [InlineKeyboardButton(text="Игры", callback_data="games")],
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=kb)
+
+def games_menu():
+    kb = [
+        [InlineKeyboardButton(text="Список игр", callback_data="list_games")],
+        [InlineKeyboardButton(text="Записать команду", callback_data="team_reg_game")],
+        [InlineKeyboardButton(text="Записаться игроком", callback_data="player_reg_game")],
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=kb)
