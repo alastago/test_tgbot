6ac5cd7b42b1569376190c2e24829e959dfdca5b
alastago <48084924+alastago@users.noreply.github.com>
2025-12-07 20:01:59 +0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,6 +11,8 @@ from config import SCHEDULE_URL, HTML_DUMP, LOGFILE
 # =========================
 # Заголовки как в браузере
 # =========================
+
+
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
