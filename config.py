30a298a7456583188063cec8dc6df5326be5ca33
alastago <48084924+alastago@users.noreply.github.com>
2025-12-07 13:42:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 TOKEN = "7666485376:AAGLUa58hLcVzu99yOJSHAzYPalRno98pTA"
+QP_URL = "https://quizplease.ru/schedule?QpGameSearch%5BcityId%5D=41&QpGameSearch%5Bdates%5D=&QpGameSearch%5Btype%5D%5B%5D=all&QpGameSearch%5Bformat%5D%5B%5D=all&QpGameSearch%5Bars%5D%5B%5D=all"
