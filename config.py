dca558aec46bc05777fcb3be5c6f8145c2ea01f9
alastago <48084924+alastago@users.noreply.github.com>
2025-12-07 20:20:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ TOKEN = "7666485376:AAGLUa58hLcVzu99yOJSHAzYPalRno98pTA"
 SCHEDULE_URL = "https://quizplease.ru/schedule?QpGameSearch%5BcityId%5D=41&QpGameSearch%5Bdates%5D=&QpGameSearch%5Btype%5D%5B%5D=all&QpGameSearch%5Bformat%5D%5B%5D=all&QpGameSearch%5Bars%5D%5B%5D=all"
 
 # Пути логов, дампа HTML, Базы даннных
-DB_PATH = "/app/databese.py"   # или твой путь
+DB_PATH = "bot.db"   # или твой путь
 LOGFILE = "/tmp/parser_quizplease.log"
 HTML_DUMP = "/tmp/quizplease_last.html"
