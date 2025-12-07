5e5738f4f47ac9df8fb7627401273864249d159a
alastago <48084924+alastago@users.noreply.github.com>
2025-12-07 14:20:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 TOKEN = "7666485376:AAGLUa58hLcVzu99yOJSHAzYPalRno98pTA"
+# URL расписания
 SCHEDULE_URL = "https://quizplease.ru/schedule?QpGameSearch%5BcityId%5D=41&QpGameSearch%5Bdates%5D=&QpGameSearch%5Btype%5D%5B%5D=all&QpGameSearch%5Bformat%5D%5B%5D=all&QpGameSearch%5Bars%5D%5B%5D=all"
+# Пути логов и дампа HTML
+LOGFILE = "/tmp/parser_quizplease.log"
+HTML_DUMP = "/tmp/quizplease_last.html"
