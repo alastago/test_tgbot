d3db897b57283b5f057cc67a344792548dcd3b41
alastago <48084924+alastago@users.noreply.github.com>
2025-12-09 23:11:51 +0700

    Update team.py

--- handlers/team.py
+++ handlers/team.py
@@ -1,9 +1,9 @@
 from aiogram import types, F
+from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from dataset.database import get_db
 from keyboards import main_menu
-
 
 # --------------------------
 # СОЗДАНИЕ КОМАНДЫ
