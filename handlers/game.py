9873be0b23e50dc34dc6ede74f4e78571fbf4199
alastago <48084924+alastago@users.noreply.github.com>
2025-12-09 23:15:11 +0700

    Update game.py

--- handlers/game.py
+++ handlers/game.py
@@ -1,4 +1,5 @@
 from aiogram import types, F
+from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from dataset.database import get_db
