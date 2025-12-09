1958a9363b146a6c1641d7d2056b653c7c6afb4f
alastago <48084924+alastago@users.noreply.github.com>
2025-12-09 21:45:41 +0700

    Update team.py

--- handlers/team.py
+++ handlers/team.py
@@ -1,4 +1,4 @@
-from aiogram import types
+from aiogram import types, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from dataset.database import get_db
