cf02aaf6344b8ac8ac2c8128a2a9732e3829e7b8
alastago <48084924+alastago@users.noreply.github.com>
2025-12-09 19:54:31 +0700

    Update game_schema.py

--- dataset/game_schema.py
+++ dataset/game_schema.py
@@ -1,9 +1,10 @@
 # game_schema.py
+from datetime import datetime
 
 GAME_SCHEMA = {
     "id": str,        # уникальный id игры (из div id)
     "datetext": str,      # дата игры
-    "date": ts,
+    "date": datetime,
     "title": str,     # название игры
     "bar": str,       # место проведения
     "price": str,     # цена
