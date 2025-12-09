d7e521fb8f5c126c1772839b646cd8808a53b075
alastago <48084924+alastago@users.noreply.github.com>
2025-12-09 18:56:04 +0700

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -2,9 +2,9 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 def main_menu():
     kb = [
+        [InlineKeyboardButton(text="Мои команды", callback_data="teams")],
         [InlineKeyboardButton(text="Создать команду", callback_data="create_team")],
         [InlineKeyboardButton(text="Вступить в команду", callback_data="join_team")],
-        [InlineKeyboardButton(text="🔍 Запустить парсер", callback_data="run_parser")],
         [InlineKeyboardButton(text="Игры", callback_data="games")],
     ]
     return InlineKeyboardMarkup(inline_keyboard=kb)
@@ -13,6 +13,6 @@ def games_menu():
     kb = [
         [InlineKeyboardButton(text="Список игр", callback_data="list_games")],
         [InlineKeyboardButton(text="Записать команду", callback_data="team_reg_game")],
-        [InlineKeyboardButton(text="Записаться игроком", callback_data="player_reg_game")],
+        
     ]
     return InlineKeyboardMarkup(inline_keyboard=kb)
