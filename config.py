98f1e5ecc96d7f200591cf75332d62febe6b2c0d
alastago <48084924+alastago@users.noreply.github.com>
2025-12-10 02:07:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "7666485376:AAGLUa58hLcVzu99yOJSHAzYPalRno98pTA"
+TOKEN = "8548002112:AAF_cMBMY-yUHyYOndwuRpFXiN2wI3sDAqs"
 # URL расписания
 SCHEDULE_URL = "https://quizplease.ru/schedule?QpGameSearch%5BcityId%5D=41&QpGameSearch%5Bdates%5D=&QpGameSearch%5Btype%5D%5B%5D=all&QpGameSearch%5Bformat%5D%5B%5D=all&QpGameSearch%5Bars%5D%5B%5D=all"
 
